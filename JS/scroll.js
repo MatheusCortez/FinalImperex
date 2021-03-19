11ff0f89b18e26713f37d9ec7e601b3d03c021ef
Matheus Cortez <matheus.cortez@live.com>
2021-03-19 00:38:41 -0300

    finalizado pagina empresa

---
+++ JS/scroll.js
@@ -0,0 +1,18 @@
+const linksInterno = document.querySelectorAll(' a[href^="#"');
+
+
+function scrollToSection(event){
+  event.preventDefault();
+  const href=event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href)
+
+
+  section.scrollIntoView({
+      behavior:'smooth',
+      block:'start'
+  });
+}
+
+linksInterno.forEach((link)=>{
+    link.addEventListener('click',scrollToSection)
+})
